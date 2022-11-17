c5b88a1e1552fbe8930e17a9e164756ad6a2ea2e
Vusal Hasanov <vusqa1999@gmail.com>
2022-11-17 11:06:14 +0400

    Restaurant

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let menuIcon=document.querySelector(".menu-icon i");
 let navbar=document.querySelector(".navbar-click")
 menuIcon.addEventListener("click",function(){
-    if(navbar.style.transform!="translateX(0px)"){
-navbar.style.transform="translateX(0px)";
+    if(navbar.style.transform!="translateX(30px)"){
+navbar.style.transform="translateX(30px)";
     }
     else{
         navbar.style.transform="translateX(200px)";
