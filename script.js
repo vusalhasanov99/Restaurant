513b3869761a1f2d1d670e98e5f7fafa1048ec45
Vusal Hasanov <vusqa1999@gmail.com>
2022-11-17 12:27:16 +0400

    Restaurant

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let menuIcon=document.querySelector(".menu-icon i");
-let navbar=document.querySelector(".navbar-click")
+let navbar=document.querySelector(".navbar-click");
+let menu=document.querySelector(".menu-icon");
+
 menuIcon.addEventListener("click",function(){
-    if(navbar.style.transform!="translateX(30px)"){
-navbar.style.transform="translateX(30px)";
+    if(navbar.style.transform!="translateX(-20px)"){
+navbar.style.transform="translateX(-20px)";
+menu.style.backgroundColor="#383845";
     }
     else{
-        navbar.style.transform="translateX(200px)";
+        navbar.style.transform="translateX(300px)";
+menu.style.backgroundColor="transparent";
+
     }
 })
